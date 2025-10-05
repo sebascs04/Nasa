46721f1d286466ac00878095fb6d025b91898cbb
sebascs04 <sebastiancamsal2002@gmail.com>
2025-10-05 16:34:51 -0500

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import DataExploration from './pages/DataExploration';
 import Results from './pages/Results';
